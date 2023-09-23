0ff6d3005aeaab21aad4a71b63a59bd751f895e4
Colin Goldberg <colin.m.goldberg@gmail.com>
2023-09-22 22:49:28 -0700

    Added more burritos

--- app/src/main/java/com/example/tortillalootbox/MainActivity.kt
+++ app/src/main/java/com/example/tortillalootbox/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.tortillalootbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tortillalootbox.ui.theme.TortillaLootboxTheme
 
@@ -35,12 +38,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    BurritoImage()
+}
+
+@Composable
+fun BurritoImage() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.burrito_open),
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TortillaLootboxTheme {
-        Greeting("Android")
+        BurritoImage()
     }
 }
